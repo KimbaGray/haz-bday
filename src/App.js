c6e9dac3daab89ee8232db06589b240baafcf9be
Kim Gray <kim.lc.gray@gmail.com>
2021-01-30 18:08:57 +0000

    added audio and photos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import image1 from "./images/image1.png";
 import image2 from "./images/image4.png";
-import haz1 from "./images/haz1.jpeg";
-import haz2 from "./images/haz2.jpg";
+import haz1 from "./images/haz1.png";
+import haz2 from "./images/haz2.png";
 import haz3 from "./images/haz3.jpg";
+import tune1 from "./media/tune1.mp3";
 import "./App.css";
 
 function App() {
@@ -13,17 +14,37 @@ function App() {
       </header>
       <div className="textBox">
         <p>
-          Looking for a birthday tune? Pump up the volume and play one of these
-          surprise tracks.
+          Ready for a birthday tune? Pump up the volume and play these surprise
+          tracks.
         </p>
       </div>
-      <p className="hazImages">
-        <img src={haz1} className="haz" alt="logo" />
-        <img src={haz2} className="haz" alt="logo" />
-        <img src={haz3} className="haz" alt="logo" />
-      </p>
+      <div className="hazImages">
+        <div className="thumbnails">
+          <img src={haz1} className="haz" alt="logo" />
+          <audio controls src={tune1} className="audio">
+            Your browser does not support the
+            <code>audio</code> element.
+          </audio>
+        </div>
+        <div className="thumbnails">
+          <img src={haz2} className="haz" alt="logo" />
+          <audio controls src={tune1} className="audio">
+            Your browser does not support the
+            <code>audio</code> element.
+          </audio>
+        </div>
+        <div className="thumbnails">
+          <img src={haz3} className="haz" alt="logo" />
+          <audio controls src={tune1} className="audio">
+            Your browser does not support the
+            <code>audio</code> element.
+          </audio>
+        </div>
+      </div>
       <footer className="footerBox">
-        <p>Wishing you a very happy and hustlin' 30th birthday, Haz!</p>
+        <p className="textBox">
+          Wishing you a very happy and hustlin' 30th birthday, Haz!
+        </p>
         <img src={image2} className="image2" alt="logo" />
       </footer>
     </div>
